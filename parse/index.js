ff28248730c98ac5ce2b95294b8a83561ffb9f2d
Nikita <grigorovich@avtoalfa.com>
2024-03-29 16:25:53 +0300

    parse: fix 'not found product'

--- parse/index.js
+++ parse/index.js
@@ -9,13 +9,16 @@ const fs = require("fs");
 const url = {
   market: "https://megamarket.ru/catalog/videokarty/#?filters=%7B%2288C83F68482F447C9F4E401955196697%22%3A%7B%22min%22%3A32302%2C%22max%22%3A85000%7D%7D",
   vCarts: `https://megamarket.ru/catalog/videokarty`,
+  kulery: "https://megamarket.ru/catalog/kulery-dlya-processora/",
+  platy: "https://megamarket.ru/catalog/materinskie-platy"
 };
 var products = [];
 
-const getMainObj = async () => {
+const getMainObj = async () =>
+{
   const browser = await puppeteer.launch({ headless: false, });
   const page = await browser.newPage();
-  
+
   // ускоряет получение данных
   page.setJavaScriptEnabled(false);
   // blockResourcesPlugin.blockedTypes.add('image');
@@ -34,35 +37,40 @@ const getMainObj = async () => {
       request.abort();
     }
   });
-  
-  for (let i = 0; i < 77; i++) {
-    const fetchUrl = `${url.vCarts}/page-${i + 1}/`
+
+  for (let i = 0; i < 77; i++)
+  {
+    const fetchUrl = `${url.platy}/page-${i + 1}/`;
     await page.goto(fetchUrl, { waitUntil: 'domcontentloaded' });
 
     const bodyScript = await page.waitForSelector("body > script:not([type],[src])");
-    
-    const mainObj = await bodyScript.evaluate((el) => {
+
+    const mainObj = await bodyScript.evaluate((el) =>
+    {
       const appObj = el.innerHTML
         .replace(/(undefined)|(window.__APP__=)/g, (val) => val === "undefined" ? "null" : "")
-        .replace(/,"experimentValue":[\W\w]+?}}/, "}");
-      return appObj ? JSON.parse(`[${appObj}]`) : [];
+        // .replace(/,"experimentValue":[\W\w]+?}}/, "}");
+        .match(/"items".+"offset":/gm)[0];
+      return appObj ? JSON.parse(`{${appObj}0}`) : {items: []};
     });
-
-    if (mainObj[0]?.hydratorState?.PlpStore?.listingData?.items?.length) products.push(...(mainObj[0].hydratorState.PlpStore.listingData.items));
+    console.log(products.length);
+    if (mainObj?.items?.length) products.push(...(mainObj.items));
     else continue;
   }
 
   (await browser).close();
-  return products; 
+  return products;
 };
 
-(async () => {
+(async () =>
+{
   const files = {
     txt: './test.txt',
     pJson: '../public/data/products.json',
   };
 
   const obj = JSON.stringify(await getMainObj());
+  // const obj = await getMainObj();
 
   fs.writeFileSync(files.pJson, obj);
 })();
