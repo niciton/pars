a1d09a8adcd4412a2d50d426751cb785a333a624
Nikita <grigorovich@avtoalfa.com>
2024-05-03 15:19:03 +0300

    appSvg: addet

---
+++ components/appSvg.vue
@@ -0,0 +1,17 @@
+<template>
+<svg :width="props.size" :height="props.size">
+  <use :xlink:href="path"></use>
+</svg>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  link: string,
+  svgId: string,
+  size: number,
+}>()
+
+const version = 1;
+
+const path = `${props.link}?=${version}#${props.svgId}`
+</script>
