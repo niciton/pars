da36ad47b340014006c746ade3f9aa439d52c5e6
Nikita <grigorovich@avtoalfa.com>
2024-05-03 15:18:21 +0300

    parse/index: edit write file

--- parse/index.js
+++ parse/index.js
@@ -1,18 +1,36 @@
-
 const puppeteer = require("puppeteer");
-// const puppeteer = require("puppeteer-extra");
-// const blockResourcesPlugin = require('puppeteer-extra-plugin-block-resources')();
-// puppeteer.use(blockResourcesPlugin);
 
 const fs = require("fs");
 
+const GlobalPath = "test";
+
+const getFilePath = (fileName = GlobalPath) => `../public/data/${fileName}.json`;
+
+const defaultPath = getFilePath(GlobalPath);
+
 const url = {
-  market: "https://megamarket.ru/catalog/videokarty/#?filters=%7B%2288C83F68482F447C9F4E401955196697%22%3A%7B%22min%22%3A32302%2C%22max%22%3A85000%7D%7D",
   vCarts: `https://megamarket.ru/catalog/videokarty`,
   kulery: "https://megamarket.ru/catalog/kulery-dlya-processora/",
   platy: "https://megamarket.ru/catalog/materinskie-platy",
+  monitory: "https://megamarket.ru/catalog/monitory/",
+  test: "https://megamarket.ru/catalog/monitory/",
 };
-var products = [];
+
+const files = {
+  txt: './test.txt',
+  pJson: getFilePath("products"),
+  monitory: getFilePath("monitory"),
+  test: getFilePath("test"),
+};
+
+fs.writeFileSync(defaultPath, "[]")
+
+function addToFile(products)
+{
+  const fileData = JSON.parse(fs.readFileSync(defaultPath));
+  fileData.push(...products);
+  fs.writeFileSync(defaultPath, JSON.stringify(fileData));
+}
 
 const getMainObj = async () =>
 {
@@ -21,10 +39,6 @@ const getMainObj = async () =>
 
   // ускоряет получение данных
   page.setJavaScriptEnabled(false);
-  // blockResourcesPlugin.blockedTypes.add('image');
-  // blockResourcesPlugin.blockedTypes.add('stylesheet');
-  // blockResourcesPlugin.blockedTypes.add('script');
-  // blockResourcesPlugin.blockedTypes.add("other");
 
   await page.setRequestInterception(true);
   page.on('request', (request) =>
@@ -38,23 +52,24 @@ const getMainObj = async () =>
     }
   });
 
-  for (let i = 0; i < 77; i++)
+  for (let i = 0; i < 400; i++)
   {
-    const fetchUrl = `${url.platy}/page-${i + 1}/`;
+    const fetchUrl = `${url[GlobalPath]}/page-${i + 1}/`;
     await page.goto(fetchUrl, { waitUntil: 'domcontentloaded' });
 
     const bodyScript = await page.waitForSelector("body > script:not([type],[src])");
 
+    // evaluate====
     const mainObj = await bodyScript.evaluate((el) =>
     {
       const appObj = el.innerHTML
         .replace(/(undefined)|(window.__APP__=)/g, (val) => val === "undefined" ? "null" : "")
-        // .replace(/,"experimentValue":[\W\w]+?}}/, "}");
         .match(/"items".+"offset":/gm)[0];
-      return appObj ? JSON.parse(`{${appObj}0}`) : {items: []};
+      return appObj ? JSON.parse(`{${appObj}0}`) : { items: [] };
     });
-    console.log(products.length);
-    if (mainObj?.items?.length) products.push(...(mainObj.items));
+    // ./evaluate====
+    
+    if (mainObj?.items?.length) addToFile(mainObj.items)
     else continue;
   }
 
@@ -64,13 +79,8 @@ const getMainObj = async () =>
 
 (async () =>
 {
-  const files = {
-    txt: './test.txt',
-    pJson: '../public/data/products.json',
-  };
+  // const obj = JSON.stringify(await getMainObj());
+  await getMainObj();
 
-  const obj = JSON.stringify(await getMainObj());
-  // const obj = await getMainObj();
-
-  fs.writeFileSync(files.pJson, obj);
+  // fs.writeFileSync(files.monitory, obj);
 })();
